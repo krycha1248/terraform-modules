8e7f989ab83def8c85ff938ccc3b8547a748af5a
Krystian Włodek <krycha1248@gmail.com>
2026-02-12 13:13:17 +0100

    updated vm-ovh provider

--- vm-ovh/main.tf
+++ vm-ovh/main.tf
@@ -1,5 +1,4 @@
 resource "openstack_compute_instance_v2" "instance" {
-  provider     = openstack.ovh
   flavour_name = var.flavour_name
   key_pair     = var.key_pair
   image_name   = var.image_name

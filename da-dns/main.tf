6ab3e1bafd26e7371004e56d132d2441219c3afa
Krystian Włodek <krycha1248@gmail.com>
2026-02-13 13:57:23 +0100

    Updated da-dns module

--- da-dns/main.tf
+++ da-dns/main.tf
@@ -1,14 +1,32 @@
 locals {
+  # Mapowanie typu rekordu do nazwy pola w API DirectAdmin
   selecttype_map = {
     A     = "arecs0"
+    AAAA  = "arecs0"
     CNAME = "cnamerecs0"
     MX    = "mxrecs0"
     NS    = "nsrecs0"
     PTR   = "ptrrecs0"
+    TXT   = "txtrecs0"
   }
+
+  # Logika budowy ciała requestu do usunięcia rekordu
+  delete_body = var.record_type == "CNAME" ? format(
+    "domain=%s&action=select&cnamerecs0=name=%s&value=%s",
+    var.domain,
+    urlencode(var.record_name),
+    urlencode(var.record_value)
+  ) : format(
+    "domain=%s&action=select&%s=name%%3D%s%%26value%%3D%s",
+    var.domain,
+    lookup(local.selecttype_map, var.record_type, "arecs0"),
+    urlencode(var.record_name),
+    urlencode(var.record_value)
+  )
 }
 
 resource "http_request" "dns_record" {
+  # DirectAdmin API endpoint
   method   = "POST"
   base_url = var.da_host
   path     = "/CMD_API_DNS_CONTROL"
@@ -19,7 +37,15 @@ resource "http_request" "dns_record" {
   }
 
   # Tworzenie rekordu
-  request_body = "domain=${var.domain}&action=add&type=${var.record_type}&name=${var.record_name}&value=${var.record_value}"
+  request_body = format(
+    "domain=%s&action=add&type=%s&name=%s&value=%s%s%s",
+    var.domain,
+    var.record_type,
+    urlencode(var.record_name),
+    urlencode(var.record_value),
+    var.record_ttl != null ? "&ttl=${var.record_ttl}" : "",
+    var.record_priority != null && var.record_type == "MX" ? "&mx_priority=${var.record_priority}" : ""
+  )
 
   is_response_body_json   = false
   response_body_id_filter = "$.id"
@@ -31,17 +57,9 @@ resource "http_request" "dns_record" {
   delete_headers    = {
     "Content-Type" = "application/x-www-form-urlencoded"
   }
+  delete_request_body = local.delete_body
 
-  delete_request_body = var.record_type == "CNAME" ? format(
-    "domain=%s&action=select&cnamerecs0=name=%s&value=%s",
-    var.domain,
-    var.record_name,
-    var.record_value
-  ) : format(
-    "domain=%s&action=select&%s=name%%3D%s%%26value%%3D%s",
-    var.domain,
-    lookup(local.selecttype_map, var.record_type, "arecs0"),
-    replace(var.record_name, ".", "%2E"),
-    var.record_value
-  )
+  # Retry w przypadku problemów z API (opcjonalnie)
+  retries = 3
+  retry_delay = 5
 }

d4f1e9d6e2f8e5c9cc5e88f7071a6628b252808f
Krystian Włodek <krycha1248@gmail.com>
2026-07-25 17:00:29 +0200

    Updated descriptions

--- k8s-cluster/outputs.tf
+++ k8s-cluster/outputs.tf
@@ -1,23 +1,28 @@
 output "kubernetes_config" {
-  value     = ovh_cloud_project_kube.cluster.kubeconfig
-  sensitive = true
+  description = "Kubernetes configuration file (kubeconfig) for the cluster"
+  value       = ovh_cloud_project_kube.cluster.kubeconfig
+  sensitive   = true
 }
 
 output "kubernetes_host" {
-  value = ovh_cloud_project_kube.cluster.kubeconfig_attributes.host
+  description = "Kubernetes API server host for the cluster"
+  value       = ovh_cloud_project_kube.cluster.kubeconfig_attributes.host
 }
 
 output "kubernetes_ca_certificate" {
-  value     = ovh_cloud_project_kube.cluster.kubeconfig_attributes.cluster_ca_certificate
-  sensitive = true
+  description = "Kubernetes API server CA certificate for the cluster"
+  value       = ovh_cloud_project_kube.cluster.kubeconfig_attributes.cluster_ca_certificate
+  sensitive   = true
 }
 
 output "kubernetes_client_certificate" {
-  value     = ovh_cloud_project_kube.cluster.kubeconfig_attributes.client_certificate
-  sensitive = true
+  description = "Kubernetes API server client certificate for the cluster"
+  value       = ovh_cloud_project_kube.cluster.kubeconfig_attributes.client_certificate
+  sensitive   = true
 }
 
 output "kubernetes_client_key" {
-  value     = ovh_cloud_project_kube.cluster.kubeconfig_attributes.client_key
-  sensitive = true
+  description = "Kubernetes API server client key for the cluster"
+  value       = ovh_cloud_project_kube.cluster.kubeconfig_attributes.client_key
+  sensitive   = true
 }

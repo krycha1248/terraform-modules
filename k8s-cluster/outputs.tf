886f08950d2a269c1038c9548b67a39b303a36f0
Krystian Włodek <krycha1248@gmail.com>
2026-07-25 16:59:25 +0200

    added outputs with connection data

--- k8s-cluster/outputs.tf
+++ k8s-cluster/outputs.tf
@@ -3,6 +3,21 @@ output "kubernetes_config" {
   sensitive = true
 }
 
-output "kubernetes_cluster_id" {
-  value = ovh_cloud_project_kube.cluster.id
+output "kubernetes_host" {
+  value = ovh_cloud_project_kube.cluster.kubeconfig_attributes.host
+}
+
+output "kubernetes_ca_certificate" {
+  value     = ovh_cloud_project_kube.cluster.kubeconfig_attributes.cluster_ca_certificate
+  sensitive = true
+}
+
+output "kubernetes_client_certificate" {
+  value     = ovh_cloud_project_kube.cluster.kubeconfig_attributes.client_certificate
+  sensitive = true
+}
+
+output "kubernetes_client_key" {
+  value     = ovh_cloud_project_kube.cluster.kubeconfig_attributes.client_key
+  sensitive = true
 }

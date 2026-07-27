fe1a1a43882f2bb67664af4f74167ca40abe5f5c
Krystian Włodek <krycha1248@gmail.com>
2026-07-27 12:39:14 +0200

    updated cloudflare domain module

--- cloudflare-domain/main.tf
+++ cloudflare-domain/main.tf
@@ -4,7 +4,7 @@ data "cloudflare_zones" "this" {
 }
 
 resource "cloudflare_dns_record" "recordA" {
-  zone_id = data.cloudflare_zones.this.id
+  zone_id = data.cloudflare_zones.this.result[0].id
   name    = var.domain_name
   type    = "A"
   content = var.ip_address
@@ -13,7 +13,7 @@ resource "cloudflare_dns_record" "recordA" {
 
 resource "cloudflare_dns_record" "recordCNAME" {
   count   = var.www_subdomain_enabled ? 1 : 0
-  zone_id = data.cloudflare_zones.this.id
+  zone_id = data.cloudflare_zones.this.result[0].id
   name    = "www.${var.domain_name}"
   type    = "CNAME"
   content = cloudflare_dns_record.recordA.name

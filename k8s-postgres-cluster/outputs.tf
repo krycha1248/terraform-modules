bc263db8f3fcaa11fe62656fc698a40038368e09
Krystian Włodek <krycha1248@gmail.com>
2026-07-28 18:30:09 +0200

    fixed output with cluster address

--- k8s-postgres-cluster/outputs.tf
+++ k8s-postgres-cluster/outputs.tf
@@ -1,3 +1,3 @@
 output "cluster_address" {
-  value = "${kubernetes_manifest.cnpg_operator.metadata.name}-rw.${kubernetes_manifest.cnpg_operator.metadata.namespace}.svc.cluster.local"
+  value = "${kubernetes_manifest.cnpg_operator.manifest.metadata.name}-rw.${kubernetes_manifest.cnpg_operator.manifest.metadata.namespace}.svc.cluster.local"
 }
